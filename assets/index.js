59df61d65e539e42c51c1ba4a8fee397dfb58e96
Theo Morales <theo.morales.fr@gmail.com>
2018-04-14 00:59:11 +0200

    Implement slide show

--- assets/index.js
+++ assets/index.js
@@ -7,24 +7,58 @@
 (function(){
   'use strict';
   
+	var photos = [
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+		'https://placehold.it/850x850',
+	];
+
    $('#start').click(function(e) {
    	   $('#header').slideUp("slow", function() {
    	   	   $('#placeholder').fadeOut('fast');
 		   $('.gaze').slideDown("slow");
-			setTimeout(3, gaze());
+		   //setListener();
+		   setTimeout(slideShow, 3000);
 	   });
    });
 
+   	var gazing = false;
+   	var i = 0;
 
-	function gaze() {
+	function setListener() {
+		console.log('setting listener');
 		webgazer.setGazeListener(function(data, elapsedTime) {
 			if (data == null)
 				return;
 
+			/*if (!gazing) {*/
+				//console.log('gazing');
+				////slideShow();
+				//gazing = true;
+			/*}*/
+
 			var xprediction = data.x; //these x coordinates are relative to the viewport
 			var yprediction = data.y; //these y coordinates are relative to the viewport
-			console.log(xprediction, yprediction);
-			console.log(elapsedTime); //elapsed time is based on time since begin was called
+			console.log(xprediction);
 		}).begin();
+	}
+
+
+	function slideShow() {
+		console.log('slideshow');
+		changeImage();
+		window.setInterval(changeImage, 10000);
+	}
+
+	function changeImage() {
+		webgazer.pause();
+		$('.gaze').fadeOut('slow');
+		$('.gaze').attr('src', photos[i++]).fadeIn('slow')
+		webgazer.resume();
 	}
 })();
