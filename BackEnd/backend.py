645866ab9ed440d3a96e82f508497ba9d36d715c
Theo Morales <theo.morales.fr@gmail.com>
2018-04-14 15:39:17 +0200

    Fix assemble()

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -4,7 +4,6 @@ import json
 import numpy as np
 import cv2
 import math
-import matplotlib.pyplot as plt
 
 from flask_cors import CORS
 from scipy.cluster.vq import kmeans
@@ -31,7 +30,7 @@ class Image:
         for c in coordinates:
             self.points.append(list(c.values()))
 
-        self.points = np.array(self.points,dtype=float)
+        self.points = np.array(self.points, dtype=float)
                 
 
     def clusterData(self,classes):
@@ -45,15 +44,11 @@ class Image:
         
         extracted_patches = []
         for mean in means:
-            x = np.asarray(mean[0] - (window_size-1)/2,dtype=int)
-            y = np.asarray(mean[1] - (window_size-1)/2,dtype=int)
-            extract = np.empty([window_size,window_size,3], dtype=int)
-            extract = self.datafile[x:x+window_size,y:y+window_size,:]
-            print(extract.shape)
-            extracted_patches.append(extract) 
-            cv2.imshow('Patch',extract)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            x = np.asarray(mean[0] - (window_size - 1) / 2, dtype=int)
+            y = np.asarray(mean[1] - (window_size - 1) / 2, dtype=int)
+            extract = np.empty([window_size, window_size, 3], dtype=int)
+            extract = self.datafile[x:x+window_size, y:y+window_size, :]
+            extracted_patches.append(extract)
 
         return extracted_patches
 
@@ -61,23 +56,20 @@ class Image:
 
     def assemble(self, patches):
         """Assemble Feature Collage"""
-        mozaiques = []
+        mosaics = []
         for i in range(5):
-            # patches = np.random.shuffle(patches)
+            # np.random.shuffle(patches)
             nb_patches_per_rows = int(math.sqrt(len(patches)))
-            nb_patches_per_cols = len(patches) / nb_patches_per_rows
-            mozaique = np.zeros([patches[0].shape[0] * nb_patches_per_rows, patches[0].shape[1] * nb_patches_per_cols, 3], dtype=int)
+            nb_patches_per_cols = int(len(patches) / nb_patches_per_rows)
+            mosaic = np.zeros([patches[0].shape[0] * nb_patches_per_rows, patches[0].shape[1] * nb_patches_per_cols, 3], dtype='uint8')
             m = n = 0
 
             for patch in patches:
-                # mozaique[n:patch.shape[0],m:patch.shape[1],:] = patch
                 l = 0
-                for j in range(n, patch.shape[0]):
+                for j in range(n * patch.shape[0], n * patch.shape[0] + patch.shape[0]):
                     p = 0
-                    for k in range(m, patch.shape[1]):
-                        # print("j={} k={} l={} p={}".format(j,k,l,p))
-                        mozaique[j, k, :] = patch[l, p, :]
-                        # print(mozaique)
+                    for k in range(m * patch.shape[1], m * patch.shape[1] + patch.shape[1]):
+                        mosaic[j, k, :] = patch[l, p, :]
                         p += 1
                     l += 1
 
@@ -86,16 +78,12 @@ class Image:
                     m = 0
                     n += 1
 
-            print(mozaique)
-            print(mozaique.shape)
-            cv2.imshow('Mozaique', mozaique)
+            cv2.imshow('mosaic', mosaic)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-            plt.imshow(mozaique)
-            plt.show()
-            mozaiques.append(mozaique)
+            mosaics.append(mosaic)
         
-        return mozaiques
+        return mosaics
 
 
     def dream(self):
@@ -114,7 +102,7 @@ def postData(imageid):
     #cv2.destroyAllWindows()
     means = x.clusterData(3)
 
-    patches = x.extract([[400,400], [256, 199], [534, 312]], 100)
+    patches = x.extract([[400,400], [256, 199], [534, 312], [135, 345], [608, 400], [702, 540]], 100)
     x.assemble(patches)
             
     
