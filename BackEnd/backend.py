ebf006047b8cbc3eeb76fd8756da5f7ab01c5c2b
Hannes Bartle <hannes@michaelbartle.de>
2018-04-14 06:25:17 +0200

    Add docstrings

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -6,12 +6,10 @@ from scipy.cluster.vq import kmeans
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-
-
-
 class Image:
 
-    def __init__(self,json_data):
+    def __init__(self,json_data,imageid):
+        self.imageid = imageid
         self.screenWidth = json_data["screenWidth"]
         self.screenHeight = json_data["screenHeight"]
         self.imgWidth = json_data["imgWidth"]
@@ -24,13 +22,22 @@ class Image:
             self.points.append(list(c.values()))
         self.points = np.array(self.points,dtype=float)
                 
-
     def clusterData(self,classes):
-        return kmeans(self.points,classes)
-
+        """Cluster Gaze Points"""
+        means = kmeans(self.points,classes)
+        return means
     
+    def extract(self)
+        """Extract Small Image Feature"""
+        return 1
 
+    def assemble(self)
+        """Assemble Feature Collage"""
+        return 1
 
+    def dream(self)
+        """Dream"""
+        return 1
 
 
 @app.route('/api/<imageid>', methods=['GET', 'POST'])
@@ -40,8 +47,8 @@ def postData(imageid):
         
         x = Image(content)
         print(x.clusterData(2))
+                
         
-
         print("Data Received!")
         return("Done!")
         
